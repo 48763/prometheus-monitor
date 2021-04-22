ab26d1ae6a6d2abecbe2202a2dd8ba022f1a6997
48763 <future.starshine@gmail.com>
2021-04-22 18:30:08 +0800

    Update init.sh

--- deploy/init.sh
+++ deploy/init.sh
@@ -37,38 +37,38 @@ fi
 WORKDIR=$(pwd)
 
 ## nginx
-${sh_c} docker images | grep -P 'nginx[[:space:]]*prom' > /dev/null 2>&1
+${sh_c} "docker images" | grep -P 'nginx[[:space:]]*prom' > /dev/null 2>&1
 if [ 1 -eq ${?} ]; then
     cd nginx
-    ${sh_c} docker build -t . nginx:prom
+    ${sh_c} "docker build -t . nginx:prom"
     cd ${WORKDIR}
 fi
 
 ## grafana
 if [ ! "472" -eq "$(stat -c %u ./grafana/grafana.db)" ]; then
-    ${sh_c} chown 472:${USER} ./grafana/grafana.db
+    ${sh_c} "chown 472:${USER} ./grafana/grafana.db"
 fi
 
 ## monitor
 if [ ! -e /data/monitor ]; then
-    ${sh_c} mkdir /data/monitor
+    ${sh_c} "mkdir /data/monitor"
 fi
 
 ## prometheus
 if [ ! -e /data/monitor/prometheus ]; then
-    ${sh_c} mkdir /data/monitor/prometheus
-    ${sh_c} chown 65534:65534 /data/monitor/prometheus
+    ${sh_c} "mkdir /data/monitor/prometheus"
+    ${sh_c} "chown 65534:65534 /data/monitor/prometheus"
 elif [ "6553465534" -eq "$(stat -c %u%g /data/monitor/prometheus)" ]; then
-    ${sh_c} chown -R 65534:65534 /data/monitor/prometheus
+    ${sh_c} "chown -R 65534:65534 /data/monitor/prometheus"
 fi
 
 ## alertmanager
 if [ ! -e /data/monitor/alertmanager ]; then
-    ${sh_c} mkdir /data/monitor/alertmanager
-    ${sh_c} chown 65534:65534 /data/monitor/alertmanager
-else [ "6553465534" -eq "$(stat -c %u%g /data/monitor/alertmanager)" ]; then
-    ${sh_c} chown -R 65534:65534 /data/monitor/alertmanager
+    ${sh_c} "mkdir /data/monitor/alertmanager"
+    ${sh_c} "chown 65534:65534 /data/monitor/alertmanager"
+elif [ "6553465534" -eq "$(stat -c %u%g /data/monitor/alertmanager)" ]; then
+    ${sh_c} "chown -R 65534:65534 /data/monitor/alertmanager"
 fi
 
 ## Change mode
-${sh_c} docker-compose up -d
+${sh_c} "docker-compose up -d"
