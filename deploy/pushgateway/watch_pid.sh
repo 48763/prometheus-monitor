3c4c81b95ab01a1c85189b110966c966404fc956
Yuki <future.starshine@gmail.com>
2022-09-29 15:12:46 +0800

    Update watch_pid.sh

--- deploy/pushgateway/watch_pid.sh
+++ deploy/pushgateway/watch_pid.sh
@@ -4,16 +4,20 @@ if [ ! ${#} -eq 4 ]; fi\\then
     exit 1
 fi
 
-ps=$(ps -eo pid,%cpu,%mem,cmd --sort %cpu,%mem | grep ${4} 2> /dev/null | grep -v "grep\|sh")
+pid_list=$(pidof ${4} | sed "s/ /\\\|/g")
+ps=$(ps -eo pid,%cpu,%mem,cmd --sort %cpu,%mem | grep -v grep | grep ${pid_list})
 
 if [ -z "${ps}" ]; then
     ps="0 -1 -1"
 fi
 
-metrics="$(echo ${ps} \
-    | awk '{
-        printf "node_process_cpu_usage{pid=\"%s\"} %s\\n" ,$1 ,$2
-        printf "node_process_memory_usage{pid=\"%s\"} %s\\n" ,$1 ,$3
-}')"
-        
-echo -e ${metrics} | curl --data-binary @- "http://localhost:9091/metrics/job/watch_pid/env/${1}/ip_address/${2}/app/${3}"
+while read line
+do
+    metrics="${metrics}$(echo ${ps} \
+        | awk '{
+            printf "node_process_cpu_usage{pid=\"%s\"} %s\\n" ,$1 ,$2
+            printf "node_process_memory_usage{pid=\"%s\"} %s\\n" ,$1 ,$3
+    }')"
+done < <(echo -e "${ps}")
+
+echo -e ${metrics} | curl --data-binary @- "http://localhost:9091/metrics/job/watch_pid/env/${1}/instance/${2}/app/${3}"
